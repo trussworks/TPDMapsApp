45d1fcb1866a7c99339115055016168e6cab4d4d
Mark Ferlatte <ferlatte@cryptio.net>
2013-09-24 17:14:23 -0700

    Add header docs.

--- TPMapsApp.h
+++ TPMapsApp.h
@@ -7,6 +7,7 @@
 
 #import <Foundation/Foundation.h>
 
+
 NS_ENUM(NSUInteger, TPMapsAppTravelMode) {
     TPMapsAppTravelModeNone = 0,
     TPMapsAppTravelModeDriving = 1,
@@ -17,21 +18,61 @@ NS_ENUM(NSUInteger, TPMapsAppTravelMode) {
 
 @interface TPMapsApp : NSObject
 
+/**
+ The name of the maps application, suitable for showing to the customer.
+ */
 @property (nonatomic, readonly) NSString *name;
+
+/**
+ If the maps application in question is installed on the device.
+ */
 @property (nonatomic, readonly, getter = isInstalled) BOOL installed;
+
+/**
+ An identifier for the maps application, suitable for storing in `NSUserDefaults`.
+ */
 @property (nonatomic, readonly) NSString *identifier;
 
+/**
+ @return An array of `TPMapsApp` objects representing every maps application that TPMapsApp knows about.
+ */
 + (NSArray *)availableMapsApps;
+
+/**
+ @return An array of `TPMapsApp' objects for maps applications that are installed.
+ */
 + (NSArray *)installedMapsApps;
+
+/**
+ @return A TPMapsApp for an identifier previously obtained from the identifier property.
+ */
 + (TPMapsApp *)mapsAppWithIdentifier:(NSString *)identifier;
 
 - (BOOL)isInstalled;
 
+/**
+ @return YES if the TPMapsApp objects refer to the same maps app, even if they are different objects.
+ */
 - (BOOL)isEqualToMapsApp:(TPMapsApp *)mapsApp;
 
-// 'Cupertino', or '37.77493,-122.419416'
+/**
+ Open the maps application and pass it the map query.
+ 
+ @param query The query string to send to the maps app (like @"Cupertino" or @"37.77493,-122.419416").
+ 
+ @return YES if the maps app opened successfully, NO otherwise.
+ */
 - (BOOL)openWithQuery:(NSString *)query;
 
+/**
+ Open the maps application and request directions between two locations.
+ 
+ @param start The query string representing the directions start location.
+ @param destination The query string representing the directions destination.
+ @param travelMode The preferred travel mode for the directions. Depending on the app used, some of these modes will not be available.
+ 
+ @return YES if the maps app opened successfully, NO otherwise.
+ */
 - (BOOL)openForDirectionsWithStart:(NSString *)start
                        destination:(NSString *)destination
                         travelMode:(enum TPMapsAppTravelMode)travelMode;
