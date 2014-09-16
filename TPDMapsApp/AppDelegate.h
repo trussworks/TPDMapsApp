d81173c30bc2c81b43f87661c12143de03789c2d
Mark Ferlatte <ferlatte@cryptio.net>
2014-09-16 16:47:28 -0700

    Initial project.

---
+++ TPDMapsApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TPDMapsApp
+//
+//  Created by Mark Ferlatte on 9/16/14.
+//  Copyright (c) 2014 Tetherpad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TPDMapsApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TPDMapsApp
+//
+//  Created by Mark Ferlatte on 9/16/14.
+//  Copyright (c) 2014 Tetherpad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
